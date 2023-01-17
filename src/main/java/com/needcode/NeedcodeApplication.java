41c157607bc8c12f0cd01439e9692c02700e1c82
pjnw1236 <pjnw1236@gmail.com>
2023-01-17 20:21:27 +0900

    initial setting

---
+++ src/main/java/com/needcode/NeedcodeApplication.java
@@ -0,0 +1,13 @@
+package com.needcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeedcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeedcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/needcode/NeedcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.needcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeedcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
