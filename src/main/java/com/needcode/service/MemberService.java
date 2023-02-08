fe749a6fde5f2e472b3edd8ad188ea727f1180b4
pjnw1236 <pjnw1236@gmail.com>
2023-02-08 16:23:51 +0900

    feat MemberService 생성

---
+++ src/main/java/com/needcode/service/MemberService.java
@@ -0,0 +1,16 @@
+package com.needcode.service;
+
+import com.needcode.entity.Member;
+import com.needcode.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member) {
+        return memberRepository.save(member);
+    }
+}
