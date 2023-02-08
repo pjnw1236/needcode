28c938ede4dfb1254d8222dcb3b76de9d4e9fefe
pjnw1236 <pjnw1236@gmail.com>
2023-02-08 17:19:10 +0900

    feat: Security Config 구성

---
+++ src/main/java/com/needcode/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.needcode.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().requestMatchers(new AntPathRequestMatcher("/**")).permitAll();
+//        .and().csrf().ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+//        .and().headers().addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
+
+        return http.build();
+    }
+}
